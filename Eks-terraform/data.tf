8b790d9e38ba6c7737d7974a1cab1a73e47243ec
iamjossi <133582723+iamjossi@users.noreply.github.com>
2024-11-22 22:49:57 +0000

    Update data.tf

--- Eks-terraform/data.tf
+++ Eks-terraform/data.tf
@@ -1,9 +1,9 @@
 # Fetch details about the EKS cluster
-data "aws_eks_cluster" "eks_details" {
+data "aws_eks_cluster" "example" {
   name = aws_eks_cluster.example.name
 }
 
 # Fetch the authentication token for the EKS cluster
-data "aws_eks_cluster_auth" "eks_auth" {
+data "aws_eks_cluster_auth" "example" {
   name = aws_eks_cluster.example.name
 }
