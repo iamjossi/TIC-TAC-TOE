bad7cdb0ce6b26b02ecf0837022cf360a425d0f0
iamjossi <133582723+iamjossi@users.noreply.github.com>
2024-11-22 17:13:05 +0000

    Update backend.tf

--- EC2-GithubActions/backend.tf
+++ EC2-GithubActions/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "your-terraform-state-bucket"
     key            = "ec2-instance/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-west-2"
   }
 }
